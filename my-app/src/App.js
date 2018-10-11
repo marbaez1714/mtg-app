e898bfd3d143825c9c740496c73c36197d010219
Mario Baez <Mario.Baez@spmflo.com>
2018-10-11 13:13:16 -0500

    CARD SEARCH IS AWESOME

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CardSearch from './pages/card-search'
 import { Grid } from 'react-bootstrap';
 import './App.css';
@@ -9,16 +10,28 @@ class App extends Component {
     this.state = {
       cardSearchState: {
         inputValue: '',
-        cardName: 'Arcane Flight',
+        cardName: 'Lets find your card!',
         cardType: 'Sorcery',
         cardImageUrl: 'https://img.scryfall.com/cards/png/en/ima/10.png?1530591620',
       },
       setSearchState: {},
     };
 
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
 
+
+  handleSearchChange(event) {
+    var newValue = this.state.cardSearchState;
+    newValue.inputValue = event.target.value
+    this.setState({ cardSearchState: newValue })
+  }
+
+  handleSearch(event) {
+    var inputVale = event.target.value;
+
+  }
 
 
 
@@ -29,7 +42,7 @@ class App extends Component {
           <h1 style={{ marginTop: '10px' }}>Ultimate MTG App</h1>
         </header>
         <Grid className="cardSearch">
-          <CardSearch cardData={this.state.cardSearchState} />
+          <CardSearch cardData={this.state.cardSearchState} changeValue={this.handleSearchChange} />
         </Grid>
       </div >
     );
--- my-app/src/pages/card-search.js
+++ my-app/src/pages/card-search.js
@@ -1,26 +1,122 @@
 import React from 'react';
-import { Row, FormControl, InputGroup, Col, Well, Panel, PanelGroup, Table } from 'react-bootstrap';
+import { Button, Row, FormControl, InputGroup, Col, Well, Panel, PanelGroup, Table } from 'react-bootstrap';
 import './styles.css'
+
+function getLegal(value) {
+    if (value === "legal") {
+        return <td className="legal"><span className="glyphicon glyphicon-ok" /></td>
+    } else {
+        return <td className="illegal"><span className="glyphicon glyphicon-remove" /></td>
+    }
+}
+
+
+
 
 function CardSearchInputs(props) {
     return (
         <Col md={12}>
             {/* Input Group */}
             <InputGroup >
-                <FormControl type="text" />
+                <FormControl type="text" onChange={props.inputChange} value={props.cardSearchState.inputValue} />
                 <InputGroup.Addon><span className="glyphicon glyphicon-search"></span></InputGroup.Addon>
             </InputGroup>
+            <p>{props.cardSearchState.inputValue}</p>
         </Col>
     )
 }
 
+function CardInfo(props) {
+    return (
+        <Panel eventKey="1" >
+            <Panel.Heading>
+                <Panel.Title toggle><h3 className="cardName">{props.cardData.cardName}</h3></Panel.Title>
+            </Panel.Heading>
+            <Panel.Body collapsible>
+                <h4 className="cardType">{props.cardData.cardType}</h4>
+
+            </Panel.Body>
+        </Panel>
+    )
+}
+
+function CardLegal(props) {
+    return (
+        <Panel eventKey="2">
+            <Panel.Heading>
+                <Panel.Title toggle><h3 className="cardName">Legality</h3></Panel.Title>
+            </Panel.Heading>
+            <Panel.Body collapsible>
+                <Col md={6} className="legalTable">
+                    <Table condensed hover bordered>
+                        <tbody>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Standard</td>
+
+                            </tr>
+                            <tr>
+                                {getLegal('not_legal')}
+                                <td>Modern</td>
+
+                            </tr>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Legacy</td>
+
+                            </tr>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Vintage</td>
+
+                            </tr>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Commander</td>
+
+                            </tr>
+                        </tbody>
+
+                    </Table>
+                </Col>
+                <Col md={6} className="legalTable">
+                    <Table condensed hover bordered>
+                        <tbody>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Frontier</td>
+                            </tr>
+                            <tr>
+                                {getLegal('not_legal')}
+                                <td>Pauper</td>
+                            </tr>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Penny</td>
+                            </tr>
+                            <tr>
+                                {getLegal('not_legal')}
+                                <td>1v1 Cmdr.</td>
+                            </tr>
+                            <tr>
+                                {getLegal('legal')}
+                                <td>Brawl</td>
+                            </tr>
+                        </tbody>
+
+                    </Table>
+                </Col>
+            </Panel.Body>
+        </Panel>
+    )
+}
 
 function CardSearch(props) {
     return (
         <div>
             <Well>
                 <Row>
-                    <CardSearchInputs />
+                    <CardSearchInputs inputChange={props.changeValue} cardSearchState={props.cardData} />
                 </Row >
                 <Row className="cardContainer">
                     <Col md={12}>
@@ -29,45 +125,10 @@ function CardSearch(props) {
                             <img className="cardSearchImage" src={props.cardData.cardImageUrl} alt="card" />
                         </Col>
                         {/* Card Information */}
-                        <Col md={7} className="cardInformation">
+                        <Col md={8} className="cardInformation">
                             <PanelGroup accordion id="accordion-uncontrolled-example" defaultActiveKey="1">
-                                <Panel eventKey="1" >
-                                    <Panel.Heading>
-                                        <Panel.Title toggle><h3 className="cardName">{props.cardData.cardName}</h3></Panel.Title>
-                                    </Panel.Heading>
-                                    <Panel.Body collapsible>
-                                        <h4 className="cardType">{props.cardData.cardType}</h4>
-
-                                    </Panel.Body>
-                                </Panel>
-                                <Panel eventKey="2">
-                                    <Panel.Heading>
-                                        <Panel.Title toggle><h3 className="cardName">Legality</h3></Panel.Title>
-                                    </Panel.Heading>
-                                    <Panel.Body collapsible>
-                                        <Table condensed hover>
-                                            <tbody>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>Mark</td>
-                                                    <td>Otto</td>
-                                                    <td>@mdo</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>2</td>
-                                                    <td>Jacob</td>
-                                                    <td>Thornton</td>
-                                                    <td>@fat</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>3</td>
-                                                    <td colSpan="2">Larry the Bird</td>
-                                                    <td>@twitter</td>
-                                                </tr>
-                                            </tbody>
-                                        </Table>
-                                    </Panel.Body>
-                                </Panel>
+                                <CardInfo cardData={props.cardData} />
+                                <CardLegal />
                             </PanelGroup>
                         </Col>
                     </Col>
